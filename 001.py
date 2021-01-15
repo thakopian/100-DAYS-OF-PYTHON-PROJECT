4971e2b3dc3bc96fb0723bbdb510e288abd4c569
Tadeh Hakopian <thakopian@gmail.com>
2021-01-15 08:23:10 -0800

    first exercise
    basic print commmand review and added the debugger

---
+++ 001.py
@@ -0,0 +1,16 @@
+# print with a new line
+print("Hello Wolrd!\nHello world!\nHello world!")
+
+# print with a concatenation
+print("Hello" + "Tadeh")
+
+# print with a concatenation and space
+print("Hello" + " Tadeh")
+print("Hello" + " " + "Tadeh")
+
+# set a variable to a string and print the variable
+msg = "hello world"
+print(msg)
+
+# create an obvious error to test debug
+print(hello world")
