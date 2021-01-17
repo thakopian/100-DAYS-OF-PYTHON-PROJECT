8b40ca7b1a8cc086bb84b35ebb7cfd17ae9dbf9d
Tadeh Hakopian <thakopian@gmail.com>
2021-01-17 09:44:39 -0800

    new python file added

---
+++ 002.py
@@ -0,0 +1,18 @@
+# input function
+# this function is like print wwith the parentheses and message inside
+# the point is to add a piece of data to the intput
+
+# example with juse the input
+""" input("what is your name?") """
+
+# tip: use ctrl+k to comment  multiple highlighted lines of code at once
+
+# input("what is your name?")
+# input("what is your name?")
+# input("what is your name?")
+# input("what is your name?")
+
+
+# example with print statement and nested inpust as a concat
+
+print("Hello " + input("what is your name"))
