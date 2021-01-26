565a20cd44ea9babac353e336d9d6a03fa632aec
Tadeh Hakopian <thakopian@gmail.com>
2021-01-26 08:20:40 -0800

    added day 3 content

---
+++ BEGIN/DAY_03/001.flow-if-else-conditional.py
@@ -0,0 +1,17 @@
+print("Can you the rollercoaster!?")
+height = int(input("What is your height in cm? "))
+
+'''
+#pseudo code
+
+if condition:
+    do this
+else:
+    do this
+'''
+
+
+if height >= 125:
+    print("Get on board and ridet he rollercoaster!!!")
+else:
+    print("sorry not good enough kid!")
