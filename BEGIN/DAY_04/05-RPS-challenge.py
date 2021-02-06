8ec4cacc4c10badfbcece15551b79bde80466d46
Tadeh Hakopian <thakopian@gmail.com>
2021-02-05 22:44:01 -0800

    Day 4 done, Day 5 start

--- BEGIN/DAY_04/05-RPS-challenge.py
+++ BEGIN/DAY_04/05-RPS-challenge.py
@@ -33,12 +33,47 @@ scissors = '''
 ---.__(___)
 '''
 
-# provide choices an an input
-
-rps_choices = [rock, paper, scissors]
-
-your_choice = input('choose by typing rock, paper, or scissor')
-
+'''
 rock = 0
 paper = 1
 scissor = 2
+'''
+
+# provide choices an an input to print them out later
+rps_choices = [rock, paper, scissors]
+
+# player input by number converted to integer
+your_choice = int(
+    input('choose by typing ROCK = 0, PAPER = 1, or SCISSOR = 2 \n> '))
+
+print("your hand")
+print(rps_choices[your_choice])
+
+# computer choice randomly generated
+comp_choice = random.randint(0, 2)
+
+print("computer hand")
+print(rps_choices[comp_choice])
+
+'''
+# test hard coded values
+your_choice = 1
+comp_choice = 2
+'''
+
+# conditional choice matrix
+
+if your_choice == 1 and comp_choice == 0:
+    print('you win')
+elif comp_choice == 1 and your_choice == 0:
+    print('you lose')
+elif your_choice == 2 and comp_choice == 1:
+    print('you win')
+elif comp_choice == 1 and your_choice == 2:
+    print('you lose')
+else:
+    print("it\'s a draw")
+
+# your outcome logic is not correct, the outcomes don't match the rules
+# account for numbers out of range stated for inputs so outcome doesn't go straight to draw
+# because the range is so important you need to reconfigure your code to make sure from the beginning the user input is in the valid range of 0-2
