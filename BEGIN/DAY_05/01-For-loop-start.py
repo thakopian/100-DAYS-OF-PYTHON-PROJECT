78e4f76bb9d8206257ae705fa4a5da69ba3fe1aa
Tadeh Hakopian <thakopian@gmail.com>
2021-02-08 07:35:26 -0800

    Day 5 for loops
    paused at day 5-1 challenge

---
+++ BEGIN/DAY_05/01-For-loop-start.py
@@ -0,0 +1,7 @@
+# beginning for loop https://repl.it/@thakopian/day-5-for-loops-end#main.py
+
+fruits = ['apple', 'peach', 'pear']
+
+for x in fruits:
+    print(x)
+    print(x + ' Pie')
---
+++ BEGIN/DAY_05/02.py
@@ -0,0 +1,16 @@
+# https://repl.it/@thakopian/day-5-1-exercise#main.py
+
+# 🚨 Don't change the code below 👇
+
+student_heights = [180, 124, 165, 173, 189, 169, 146].split()
+
+# student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+
+# 🚨 Don't change the code above 👆
+
+
+# for x in student_heights:
+
+print(n)
